cae99dc66cea8dabb9a434d38dd50cc4fefe44b2
cis101 <cis101@secs.iub.edu.bd>
2019-10-02 09:06:24 +0600

    Task 4

--- script.js
+++ script.js
@@ -38,4 +38,40 @@ function validEmail(email){
     */
 }
 
-validEmail("yaminkhan017@gmail.com")
+if(validEmail("yaminkhan017@gmail.com")){
+    console.log("You have entered a valid email")
+}
+
+
+//Task 4
+
+var library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true,
+        updateStatus: function (read){ this.readingStatus = read}
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true,
+        updateStatus: function (read){ this.readingStatus = read}
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false,
+        updateStatus: function (read){ this.readingStatus = read}
+    }];
+
+    console.log(library[1].readingStatus)
+    library[1].updateStatus(false);
+    console.log("reading status",library[1].readingStatus)
+
+
+    function readStatus(index){
+        return library[index].readingStatus
+    }
+
+    console.log(readStatus(2))
